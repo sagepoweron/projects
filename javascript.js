d0fb296c69eb8c891267be0bae29023dfa2d25ba
sagepoweron <108161754+sagepoweron@users.noreply.github.com>
2022-09-06 13:27:35 -0400

    Initial Files

---
+++ javascript.js
@@ -0,0 +1,30 @@
+var iframe = document.getElementById("iframe_game");
+function LoadGame()
+{
+iframe.src = "https://sagepoweron.github.io/octosplat/";
+}
+
+//let message;
+//message = 'Welcome!';
+//alert(message); // shows the variable content
+
+//let number = prompt('Pick a number.', 18);
+//alert(number);
+
+//let result = multiply(3, 4);
+//alert(result);
+
+function showMessage()
+{
+alert(message);
+}
+
+function add7(number)
+{
+return number + 7;
+}
+
+function multiply(number1, number2)
+{
+return number1 * number2;
+}
