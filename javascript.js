9badf83bbdfdd5c6a11a6c18009415521759064c
sagepoweron <108161754+sagepoweron@users.noreply.github.com>
2025-04-10 22:54:42 -0400

    Changed navigation bar and added images

--- javascript.js
+++ javascript.js
@@ -1,8 +1,36 @@
-var iframe = document.getElementById("iframe");
+
+const iframe = document.getElementById("iframe");
 function LoadOctosplat()
 {
 iframe.src = "https://sagepoweron.github.io/octosplat/";
 }
+
+function LoadImages(images, containerId)
+{
+    const container = document.getElementById(containerId);
+    images.map(src =>
+    {
+        const figure = document.createElement('figure');
+
+        const img = document.createElement('img');
+        img.src = src;
+
+        figure.appendChild(img);
+        container.appendChild(figure);
+    });
+
+    /*for (let index = 0; index < images.length; index++)
+    {
+        var img = document.createElement("img");
+        img.src = images[index];
+        container.appendChild(img);
+    }*/
+}
+
+window.onclick = () => alert('Welcome!');
+
+//$("#iframe").contents().find("canvas").css('cursor', 'default');
+
 
 //let message;
 //message = 'Welcome!';
@@ -16,15 +44,10 @@ iframe.src = "https://sagepoweron.github.io/octosplat/";
 
 function showMessage()
 {
-alert(message);
-}
-
-function add7(number)
-{
-return number + 7;
+    alert(message);
 }
 
 function multiply(number1, number2)
 {
-return number1 * number2;
+    return number1 * number2;
 }
